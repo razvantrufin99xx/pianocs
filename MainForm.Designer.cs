8ad5801af87f018835ff3c9cb2cc2b389dcf4296
Răzvan Trufin <144681144+razvantrufin99xx@users.noreply.github.com>
2024-05-27 21:29:37 +0300

    Add files via upload
    use audacity to split more wave files with songs and add more notes

---
+++ MainForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 5/27/2024
+ * Time: 10:39 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace musicaSoundPlayer
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.White;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.ForeColor = System.Drawing.Color.Black;
+			this.button1.Location = new System.Drawing.Point(275, 68);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(51, 191);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "DO";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.White;
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.ForeColor = System.Drawing.Color.Black;
+			this.button2.Location = new System.Drawing.Point(328, 68);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(51, 191);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "RE";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.White;
+			this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.ForeColor = System.Drawing.Color.Black;
+			this.button3.Location = new System.Drawing.Point(381, 68);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(51, 191);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "MI";
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.White;
+			this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.ForeColor = System.Drawing.Color.Black;
+			this.button4.Location = new System.Drawing.Point(434, 68);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(51, 191);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "FA";
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.White;
+			this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.ForeColor = System.Drawing.Color.Black;
+			this.button5.Location = new System.Drawing.Point(487, 68);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(51, 191);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "SO";
+			this.button5.UseVisualStyleBackColor = false;
+			this.button5.Click += new System.EventHandler(this.Button5Click);
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.Color.White;
+			this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button6.ForeColor = System.Drawing.Color.Black;
+			this.button6.Location = new System.Drawing.Point(540, 68);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(51, 191);
+			this.button6.TabIndex = 5;
+			this.button6.Text = "LA";
+			this.button6.UseVisualStyleBackColor = false;
+			this.button6.Click += new System.EventHandler(this.Button6Click);
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.Color.White;
+			this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button7.ForeColor = System.Drawing.Color.Black;
+			this.button7.Location = new System.Drawing.Point(593, 68);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(51, 191);
+			this.button7.TabIndex = 6;
+			this.button7.Text = "SI";
+			this.button7.UseVisualStyleBackColor = false;
+			this.button7.Click += new System.EventHandler(this.Button7Click);
+			// 
+			// button8
+			// 
+			this.button8.BackColor = System.Drawing.Color.White;
+			this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button8.ForeColor = System.Drawing.Color.Black;
+			this.button8.Location = new System.Drawing.Point(646, 68);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(51, 191);
+			this.button8.TabIndex = 7;
+			this.button8.Text = "DO";
+			this.button8.UseVisualStyleBackColor = false;
+			this.button8.Click += new System.EventHandler(this.Button8Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1096, 319);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "musicaSoundPlayer";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 5/27/2024
+ * Time: 10:39 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+
+namespace musicaSoundPlayer
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		SoundPlayer s ;
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+	
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+				s = new SoundPlayer("do.wav");
+				s.Play();
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+	s = new SoundPlayer("re.wav");
+				s.Play();
+		}
+		void Button3Click(object sender, EventArgs e)
+		{
+	s = new SoundPlayer("mi.wav");
+				s.Play();
+		}
+		void Button4Click(object sender, EventArgs e)
+		{
+	s = new SoundPlayer("fa.wav");
+				s.Play();
+		}
+		void Button5Click(object sender, EventArgs e)
+		{
+	s = new SoundPlayer("so.wav");
+				s.Play();
+		}
+		void Button6Click(object sender, EventArgs e)
+		{
+	s = new SoundPlayer("la.wav");
+				s.Play();
+		}
+		void Button7Click(object sender, EventArgs e)
+		{
+	s = new SoundPlayer("si.wav");
+				s.Play();
+		}
+		void Button8Click(object sender, EventArgs e)
+		{
+	s = new SoundPlayer("dosus.wav");
+				s.Play();
+		}
+	
+	}
+}
